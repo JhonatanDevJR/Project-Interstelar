a06de2d62ae3993cc914f7ab13880be117c80b9a
JhonatanDevJR <hjhonatan90@gmail.com>
2024-11-02 16:30:48 -0300

    att

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var img= document.getElementById('foto')
 var text1= document.getElementById('text1')
-function verificar(){
-    
-    var text2= document.getElementById('text2')
-    
-    
+var text2= document.getElementById('text2')
+function entrou1(){
     if(text1){
-        text1.innerHTML=`<p>OLÁ MUNDO!</p>`
-        
-    } else if(text2){
-        text2.innerHTML=`CUCUUCUCUUC`
-    }
-}  
-function sair(){
-    img.innerHTML=`<p>HELLO WORLD!</p>`
-    
-    
+        text1.innerHTML=`<p>Mercúrio é o planeta mais próximo do Sol e o menor do Sistema Solar. É o primeiro planeta do Sistema Solar, a contar da proximidade com o Sol, distando-se em apenas 57,9 milhões de quilômetros da estrela. As primeiras observações com telescópio datam de 1610, e Galileu Galilei fez as primeiras constatações. Um dia em Mercúrio dura 59 dias na Terra. Um ano em Mercúrio dura 88 dias na Terra. A velocidade de rotação de Mercúrio pode atingir 180 mil km/hora.</p>`} 
 }
-    
+function entrou2(){
+    text2.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou3(){
+    text3.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou4(){
+    text4.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou5(){
+    text5.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou6(){
+    text6.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou7(){
+    text7.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
+function entrou8(){
+    text8.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+}
