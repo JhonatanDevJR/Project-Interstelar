ce921da89778858d090f241a5488c37e3b4df95d
JhonatanDevJR <hjhonatan90@gmail.com>
2024-11-02 14:20:55 -0300

    att

---
+++ script.js
@@ -0,0 +1,20 @@
+var img= document.getElementById('foto')
+var text1= document.getElementById('text1')
+function verificar(){
+    
+    var text2= document.getElementById('text2')
+    
+    
+    if(text1){
+        text1.innerHTML=`<p>OLÁ MUNDO!</p>`
+        
+    } else if(text2){
+        text2.innerHTML=`CUCUUCUCUUC`
+    }
+}  
+function sair(){
+    img.innerHTML=`<p>HELLO WORLD!</p>`
+    
+    
+}
+    
