1a1cddbae7dbe7d114e430cf2532d34272d1ee63
JhonatanDevJR <hjhonatan90@gmail.com>
2024-11-02 23:54:20 -0300

    att

--- script.js
+++ script.js
@@ -1,38 +1,75 @@
 var img= document.getElementById('foto')
 var text1= document.getElementById('text1')
-var text2= document.getElementById('text2')
 var test= document.getElementById('teste')
-//function entrou1(){
-        //text1.innerHTML=`<p>Mercúrio é o planeta mais próximo do Sol e o menor do Sistema Solar. É o primeiro planeta do Sistema Solar, a contar da proximidade com o Sol, distando-se em apenas 57,9 milhões de quilômetros da estrela. As primeiras observações com telescópio datam de 1610, e Galileu Galilei fez as primeiras constatações. Um dia em Mercúrio dura 59 dias na Terra. Um ano em Mercúrio dura 88 dias na Terra. A velocidade de rotação de Mercúrio pode atingir 180 mil km/hora.</p>` 
+function entrou1(){
+    text1.innerHTML='<p>Mercúrio é o planeta mais próximo do Sol e o menor do Sistema Solar. É o primeiro planeta do Sistema Solar, a contar da proximidade com o Sol, distando-se em apenas 57,9 milhões de quilômetros da estrela. As primeiras observações com telescópio datam de 1610, e Galileu Galilei fez as primeiras constatações. Um dia em Mercúrio dura 59 dias na Terra. Um ano em Mercúrio dura 88 dias na Terra. A velocidade de rotação de Mercúrio pode atingir 180 mil km/hora.</p>'
+    document.getElementById("foto1").style.display='none';
+}
+function saiu1(){
+    text1.innerHTML='clique na imagem.'
+    document.getElementById("foto1").style.display ='block'; 
+} 
 
 function entrou2(){
     text2.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    document.getElementById("foto2").style.display='none';
 }
+function saiu2(){
+    text2.innerHTML='clique na imagem.'
+    document.getElementById("foto2").style.display ='block'; 
+} 
+
 function entrou3(){
-    text3.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text3.innerHTML=`<p>O Planeta Terra é um dos planetas que fazem parte do Sistema Solar e é o terceiro planeta mais próximo do Sol. A sua formação ocorreu há bilhões de anos, assim como a existência de vida aqui. Algumas teorias explicam sua origem, como a teoria da nebulosa solar.</p>`
+    document.getElementById("foto3").style.display='none';
 }
+function saiu3(){
+    text3.innerHTML='clique na imagem.'
+    document.getElementById("foto3").style.display ='block'; 
+} 
+
 function entrou4(){
-    text4.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text4.innerHTML=`<p>Marte é o quarto planeta do Sistema Solar, ficando entre a Terra e Júpiter. A 227 milhões de km do Sol, é um planeta formado por superfície rochosa e atmosfera rica em CO2. Representação de Marte, quarto planeta a partir do Sol. Em função de sua coloração, é chamado também de Planeta Vermelho. Possui o maior vulcão de todo o nosso sistema, uma superfície vermelha, e até estações como verão e inverno.</p>`
+    document.getElementById("foto4").style.display='none';
 }
+function saiu4(){
+    text4.innerHTML='clique na imagem.'
+    document.getElementById("foto4").style.display ='block'; 
+} 
+
 function entrou5(){
-    text5.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text5.innerHTML=`<p>Júpiter é um gigante gasoso e o maior planeta do Sistema Solar. Júpiter é o quinto planeta do Sistema Solar a partir do Sol. Trata-se de um gigante gasoso que apresenta superfície formada pelos gases hélio e hidrogênio, em sua maioria, com uma ampla atmosfera e um núcleo sólido ou pastoso em seu interior.</p>`
+    document.getElementById("foto5").style.display='none';
 }
+function saiu5(){
+    text5.innerHTML='clique na imagem.'
+    document.getElementById("foto5").style.display ='block'; 
+} 
+
 function entrou6(){
-    text6.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text6.innerHTML=`<p>Saturno é um planeta gasoso do Sistema Solar, o segundo maior e o sexto mais distante do Sol. É conhecido por seus anéis, que são visíveis da Terra. É o planeta com mais luas do Sistema Solar, com 146 Tem uma superfície com tons amarelados, marrons e brancos. É composto principalmente por hidrogênio e hélio, mas também contém amônia e água congelada.</p>`
+    document.getElementById("foto6").style.display='none';
 }
+function saiu6(){
+    text6.innerHTML='clique na imagem.'
+    document.getElementById("foto6").style.display ='block'; 
+} 
+
 function entrou7(){
-    text7.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text7.innerHTML=`<p>Urano é um planeta gasoso do Sistema Solar, o terceiro maior e o quarto mais massivo. É o planeta mais frio do Sistema Solar, com temperaturas que podem chegar a -224°C. Tem uma coloração azul clara, devido à presença de metano em sua atmosfera. É conhecido como o "planeta inclinado" porque sua rotação é no sentido contrário aos demais planetas, com uma inclinação de 98° em relação ao plano de sua órbita. </p>`
+    document.getElementById("foto7").style.display='none';
 }
+function saiu7(){
+    text7.innerHTML='clique na imagem.'
+    document.getElementById("foto7").style.display ='block'; 
+} 
+
 function entrou8(){
-    text8.innerHTML=`<p>Vênus é um planeta rochoso do sistema solar, o segundo mais próximo do Sol, e é conhecido por ser o planeta irmão da Terra. Vênus é semelhante à Terra em termos de tamanho e massa, sendo apenas 650 km menor em diâmetro e 81,5% da massa da Terra. A atmosfera de Vênus é muito densa e composta por 96,5% de dióxido de carbono, o que dá ao planeta uma cor amarelada.</p>`
+    text8.innerHTML=`<p>Netuno é um planeta gasoso pertencente ao Sistema Solar classificado como um gigante de gelo. Trata-se do oitavo planeta a partir do Sol, sendo, portanto, o planeta mais distante do Sol. Em decorrência disso, o tempo que Netuno leva para dar uma volta completa em torno dessa estrela é muito grande: 165 anos.</p>`
+    document.getElementById("foto8").style.display='none';
 }
-function entrou1(){
-    text1.innerHTML='<p>Mercúrio é o planeta mais próximo do Sol e o menor do Sistema Solar. É o primeiro planeta do Sistema Solar, a contar da proximidade com o Sol, distando-se em apenas 57,9 milhões de quilômetros da estrela. As primeiras observações com telescópio datam de 1610, e Galileu Galilei fez as primeiras constatações. Um dia em Mercúrio dura 59 dias na Terra. Um ano em Mercúrio dura 88 dias na Terra. A velocidade de rotação de Mercúrio pode atingir 180 mil km/hora.</p>'
-    document.getElementById("foto").style.display='none';
-    
-}
-function sair(){
-    text1.innerHTML='clique na imagem.'
-    document.getElementById("foto").style.display ='block';
-    
-}
+function saiu8(){
+    text8.innerHTML='clique na imagem.'
+    document.getElementById("foto8").style.display ='block'; 
+} 
+
